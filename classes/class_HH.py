32c805363a1b6fe76e43e11ca50fbe0ad5025807
Blendi <dronramone@mail.ru>
2023-04-09 18:44:40 +0400

    Реализован класс обработки запросов HH

---
+++ classes/class_HH.py
@@ -0,0 +1,34 @@
+from classes.abstract_class import Servises
+import requests
+import json
+
+
+class HH_request(Servises):
+    #__slots__ = ('url', 'json_file_name')
+
+    def __init__(self):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.json_file_name = '../side_file/result.json'
+
+    @property
+    def params(self) -> dict:
+        """Создать атрибут params"""
+        params = {
+            'text': 'python',
+            'per_page': 50,
+            'area': 113
+        }
+        return params
+
+    def get_data(self) -> requests.Response:
+        """Запрос"""
+        respon = requests.get(
+            url=self.url,
+            params=self.params,
+        )
+        return respon
+
+    def create_file(self) -> None:
+        with open(self.json_file_name, 'w', encoding='utf-8') as file:
+            data = self.get_data().json()
+            json.dump(data, file, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -4,20 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    params = {
-        'text': 'python',
-        'per_page': 50,
-        'area': 113
-    }
-
-    respon = requests.get(
-        url='https://api.hh.ru/vacancies',
-        params=params,
-    )
-
-    with open('result.json', 'w', encoding='utf-8') as file:
-        a = respon.json()
-        json.dump(a, file, ensure_ascii=False, indent=4)
+    pass
 
 
 if __name__ == '__main__':
