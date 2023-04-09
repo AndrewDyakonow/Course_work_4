c0a109ed23cedd610752ee9c82f332c8c5f0145e
Blendi <dronramone@mail.ru>
2023-04-09 16:49:19 +0400

    Реализован запрос

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
 def main():
-    pass
+    params = {
+        'text': 'python',
+        'per_page': 50,
+        'area': 113
+    }
+
+    respon = requests.get(
+        url='https://api.hh.ru/vacancies',
+        params=params,
+    )
+
+    with open('result.json', 'w', encoding='utf-8') as file:
+        a = respon.json()
+        json.dump(a, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
